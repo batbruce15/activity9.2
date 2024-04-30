8c6fbc1428eb92da1fab6eabc27e9ae44a696d0d
karim.battou20@gmail.com <karim.battou20@gmail.com>
2024-04-30 11:11:01 +0200

    la methode run commenté

--- src/main/java/com/activty9/activty9/Activty9Application.java
+++ src/main/java/com/activty9/activty9/Activty9Application.java
@@ -25,41 +25,41 @@ public class Activty9Application implements CommandLineRunner {
 		SpringApplication.run(Activty9Application.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO : récupérer la liste de tous les patients puis afficher leurs noms done
-		List<patient> patients = pMapper.getAll();
-		System.out.println("Liste de tous les patients :");
-		for (patient p : patients) {
-			System.out.println(p);
-		}
-
-		// TODO : rechercher les patients ayant le nom "Yahi" puis leurs prénoms
-		String nomRecherche = "Yahi";
-		List<patient> patientsYahi = pMapper.getByName(nomRecherche);
-		System.out.println("Patients ayant le nom \"" + nomRecherche + "\" :");
-		for (patient p : patientsYahi) {
-			System.out.println("Nom: " + p + ", Prénom: " + p.getPrenom());
-		}
-
-		// TODO : créer un nouveau patient (valeurs au choix)  PUIS enregistrer-le
-		patient nouveauPatient = new patient();
-		nouveauPatient.setId(62);
-		nouveauPatient.setNom("Karim");
-		nouveauPatient.setPrenom("Bat");
-		nouveauPatient.setSexe("m");
-		nouveauPatient.setAdresse("Alger");
-		Date dateNaissance = new Date(94, 0, 5);
-		nouveauPatient.setDate_naissance(dateNaissance );
-		nouveauPatient.setNumero_telephone("066666666666");
-		// Set other attributes as needed
-		pMapper.insert(nouveauPatient);
-		System.out.println("Nouveau patient ajouté.");
-
-		// TODO : rechercher la consultation ayant id=3
-		int idConsultationRecherche = 3;
-		consultation c = cMapper.getById(idConsultationRecherche);
-		System.out.println("Consultation avec ID=" + idConsultationRecherche + " :");
-		System.out.println(c);
-	}
+//	@Override
+//	public void run(String... args) throws Exception {
+//		// TODO : récupérer la liste de tous les patients puis afficher leurs noms done
+//		List<patient> patients = pMapper.getAll();
+//		System.out.println("Liste de tous les patients :");
+//		for (patient p : patients) {
+//			System.out.println(p);
+//		}
+//
+//		// TODO : rechercher les patients ayant le nom "Yahi" puis leurs prénoms
+//		String nomRecherche = "Yahi";
+//		List<patient> patientsYahi = pMapper.getByName(nomRecherche);
+//		System.out.println("Patients ayant le nom \"" + nomRecherche + "\" :");
+//		for (patient p : patientsYahi) {
+//			System.out.println("Nom: " + p + ", Prénom: " + p.getPrenom());
+//		}
+//
+//		// TODO : créer un nouveau patient (valeurs au choix)  PUIS enregistrer-le
+//		patient nouveauPatient = new patient();
+//		nouveauPatient.setId(62);
+//		nouveauPatient.setNom("Karim");
+//		nouveauPatient.setPrenom("Bat");
+//		nouveauPatient.setSexe("m");
+//		nouveauPatient.setAdresse("Alger");
+//		Date dateNaissance = new Date(94, 0, 5);
+//		nouveauPatient.setDate_naissance(dateNaissance );
+//		nouveauPatient.setNumero_telephone("066666666666");
+//		// Set other attributes as needed
+//		pMapper.insert(nouveauPatient);
+//		System.out.println("Nouveau patient ajouté.");
+//
+//		// TODO : rechercher la consultation ayant id=3
+//		int idConsultationRecherche = 3;
+//		consultation c = cMapper.getById(idConsultationRecherche);
+//		System.out.println("Consultation avec ID=" + idConsultationRecherche + " :");
+//		System.out.println(c);
+//	}
 }
